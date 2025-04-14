64b9d92028539201d848e111a0d736a8b9736074
H <3794hyo@gmail.com>
2025-04-14 14:46:20 +0900

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Footer, Layout, Navbar } from "nextra-theme-docs";
 import { Banner, Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
-import Link from "next/link";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   // Define your metadata here
@@ -46,6 +46,7 @@ export default async function RootLayout({ children }) {
         >
           {children}
         </Layout>
+        <Analytics />
       </body>
     </html>
   );
